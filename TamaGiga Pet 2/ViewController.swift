d09db75110043365399ec207580d35852275e762
Jason McCoy <jasonmccoy@jasons-macbook-pro.local>
2016-06-26 00:22:25 -0700

    Initial Commit

---
+++ TamaGiga Pet 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TamaGiga Pet 2
+//
+//  Created by Jason McCoy on 6/26/16.
+//  Copyright © 2016 Jason McCoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
