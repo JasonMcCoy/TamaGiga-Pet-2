d09b568efe14b00c810bcb7b027c2c0f464eb40f
Jason McCoy <jasonmccoy@Jasons-MacBook-Pro.local>
2016-07-03 03:05:06 -0700

    Implemented Restart Button when the game is over
    When the golem dies a restart button will appear in the middle of the
    screen. Upon pressing that button, you will be taken back to the same
    screen as initially launched.

--- TamaGiga Pet 2/ViewController.swift
+++ TamaGiga Pet 2/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
@@ -18,6 +19,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var penaltySkullImg1: UIImageView!
     @IBOutlet weak var penaltySkullImg2: UIImageView!
     @IBOutlet weak var penaltySkullImg3: UIImageView!
+    @IBOutlet weak var restartBtn: UIButton!
+    
+    @IBAction func restartBtn(_ sender: AnyObject) {
+        
+        penalties = 0
+        monsterHappy = false
+        currentItem = 0
+        
+        penaltySkullImg1.alpha = DIM_ALPHA
+        penaltySkullImg2.alpha = DIM_ALPHA
+        penaltySkullImg3.alpha = DIM_ALPHA
+        
+        redGolem.playIdleAnimation()
+        
+        restartBtn.isHidden = true
+        startGame()
+        
+    }
     
     
     let OPAQUE: CGFloat = 1.0
@@ -29,6 +48,13 @@ class ViewController: UIViewController {
     var timer: Timer!
     var monsterHappy = false
     var currentItem: UInt32 = 0
+    
+    var sfxBGMusic: AVAudioPlayer!
+    var sfxSkull: AVAudioPlayer!
+    var sfxHeart: AVAudioPlayer!
+    var sfxBite: AVAudioPlayer!
+    var sfxStalactite: AVAudioPlayer!
+    var sfxDeath: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,9 +70,7 @@ class ViewController: UIViewController {
         penaltySkullImg2.alpha = DIM_ALPHA
         penaltySkullImg3.alpha = DIM_ALPHA
         
-        NotificationCenter.default().addObserver(self, selector: #selector(itemDroppedOnCharacter), name: "onTargetDropped", object: nil)
-        
-        startTimer()
+        startGame()
         
     }
     
@@ -60,19 +84,40 @@ class ViewController: UIViewController {
         redGolemHeart.isUserInteractionEnabled = false
         redGolemStalactite.alpha = DIM_ALPHA2
         redGolemStalactite.isUserInteractionEnabled = false
+        
+        if currentItem == 0 { // 0 means the Heart food is playable and the other foods are not.
+            sfxHeart.play()
+        } else if currentItem == 1 { // 1 means the Meat food is playable and the other foods are not.
+            sfxBite.play()
+        } else if currentItem == 2 { // 2 means the Stalactite food is playable and the other foods are not.
+            sfxStalactite.play()
+        }
     }
     
     func startTimer() {
         if timer != nil {
-            timer.invalidate()
+        timer.invalidate()
         }
         timer = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(ViewController.changeGameState), userInfo: nil, repeats: true)
+    }
+    
+    func restartTimer() {
+        if timer != nil {
+            timer.invalidate()
+        }
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(ViewController.restartButton), userInfo: nil, repeats: true)
+    }
+    
+    func restartButton() {
+        restartBtn.isHidden = false
     }
     
     func changeGameState() {
         
         if !monsterHappy {
-                    penalties += 1
+            penalties += 1
+            
+            sfxSkull.play()
         
         if penalties == 1 {
             penaltySkullImg1.alpha = OPAQUE
@@ -129,10 +174,40 @@ class ViewController: UIViewController {
         
 }
     
+    func startGame() {
+        NotificationCenter.default().addObserver(self, selector: #selector(itemDroppedOnCharacter), name: "onTargetDropped", object: nil)
+        
+        do {
+            
+            try sfxBGMusic = AVAudioPlayer(contentsOf: URL(fileURLWithPath: Bundle.main().pathForResource("bgmusic", ofType: "mp3")!))
+            try sfxSkull = AVAudioPlayer(contentsOf: URL(fileURLWithPath: Bundle.main().pathForResource("skull", ofType: "wav")!))
+            try sfxHeart = AVAudioPlayer(contentsOf: URL(fileURLWithPath: Bundle.main().pathForResource("heart", ofType: "wav")!))
+            try sfxBite = AVAudioPlayer(contentsOf: URL(fileURLWithPath: Bundle.main().pathForResource("bite", ofType: "wav")!))
+            try sfxStalactite = AVAudioPlayer(contentsOf: URL(fileURLWithPath: Bundle.main().pathForResource("stalactite", ofType: "mp3")!))
+            try sfxDeath = AVAudioPlayer(contentsOf: URL(fileURLWithPath: Bundle.main().pathForResource("death", ofType: "wav")!))
+            
+            sfxBGMusic.prepareToPlay()
+            sfxBGMusic.play()
+            
+            sfxSkull.prepareToPlay()
+            sfxHeart.prepareToPlay()
+            sfxBite.prepareToPlay()
+            sfxStalactite.prepareToPlay()
+            sfxDeath.prepareToPlay()
+            
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+        
+        startTimer()
+    }
+    
     func gameOver() {
         timer.invalidate()
         redGolem.playDeathAnimation()
         pinkGolem.playDeathAnimation()
+        sfxDeath.play()
+        restartTimer()
     }
     
     /* Hide Status Bar */
